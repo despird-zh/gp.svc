068f4e519cf3868c2abb78f53c674ad29d5cc680
gary-dgc <garydiao@localhost>
2016-05-21 18:14:11 +0800

    fix copyright information

--- src/main/java/com/gp/common/IdKey.java
+++ src/main/java/com/gp/common/IdKey.java
@@ -1,3 +1,18 @@
+/*******************************************************************************
+ * Copyright 2016 Gary Diao - gary.diao@outlook.com
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *******************************************************************************/
 package com.gp.common;
 
 import java.util.HashSet;
@@ -55,6 +70,9 @@ public enum IdKey implements Identifier{
 	ROLE_PAGE("gp_role_page","rel_id", Integer.class),
 	USER_ROLE("gp_user_role","rel_id", Long.class);
 
+	/**
+	 * the custom Identifier set 
+	 **/
 	private static Set<Identifier> IdSet = new HashSet<Identifier>();
 	
 	private final String schema;
@@ -110,11 +128,18 @@ public enum IdKey implements Identifier{
 	        ));
 	}
 	
+	/**
+	 * Get the custom Identifier set
+	 *  
+	 **/
 	public static Set<Identifier> getIdentifierSet(){
 		
 		return IdSet;
 	}
 	
+	/**
+	 * Add the custom Identifier  
+	 **/
 	public static void addIdentifier(Identifier idkey){
 		
 		IdSet.add(idkey);
