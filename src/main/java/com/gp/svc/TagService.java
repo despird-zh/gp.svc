407ac14eca7dc50fe0bbe2169bbcab0da64e3722
gary-dgc <garydiao@localhost>
2016-05-30 13:48:57 +0800

    add method find tags of multiple objects

--- src/main/java/com/gp/svc/TagService.java
+++ src/main/java/com/gp/svc/TagService.java
@@ -1,6 +1,8 @@
 package com.gp.svc;
 
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import com.gp.common.ServiceContext;
 import com.gp.exception.ServiceException;
@@ -23,6 +25,14 @@ public interface TagService {
 	 * Get the file tag list 
 	 **/
 	public List<TagInfo> getTags(ServiceContext<?> svcctx, String category, InfoId<?> objectId) throws ServiceException;
+	
+	/**
+	 * Get the tags of objects
+	 * @param objectIds the array of objectId i.e. the resource_id in tag_rel
+	 * @return the map of tag set.
+	 *  
+	 **/
+	public Map<InfoId<?>, Set<TagInfo>> getTags(ServiceContext<?> svcctx, InfoId<?> ... objectIds) throws ServiceException;
 	
 	/**
 	 * create new tag record
--- src/main/java/com/gp/svc/impl/TagServiceImpl.java
+++ src/main/java/com/gp/svc/impl/TagServiceImpl.java
@@ -1,15 +1,23 @@
 package com.gp.svc.impl;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.lang.ArrayUtils;
 import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.RowCallbackHandler;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,6 +29,7 @@ import com.gp.dao.PseudoDAO;
 import com.gp.dao.TagDAO;
 import com.gp.dao.TagRelDAO;
 import com.gp.exception.ServiceException;
+import com.gp.info.Identifier;
 import com.gp.info.InfoId;
 import com.gp.info.TagInfo;
 import com.gp.info.TagRelInfo;
@@ -106,6 +115,68 @@ public class TagServiceImpl implements TagService{
 		return result;
 	}
 
+	public Map<InfoId<?>, Set<TagInfo>> getTags(ServiceContext<?> svcctx, InfoId<?> ... objectIds) throws ServiceException {
+		
+		final Map<InfoId<?>,Set<TagInfo>> result = new HashMap<InfoId<?>,Set<TagInfo>>();
+		
+		Map<String, Object> paramap = new HashMap<String, Object>();
+		StringBuffer SQL = new StringBuffer();
+		
+		SQL.append("SELECT rel.rel_id, tag.tag_id,rel.resource_id,rel.resource_type, ");
+		SQL.append(" tag.tag_type, rel.tag_name, ");
+		SQL.append(" rel.category, tag.tag_color, ");
+		SQL.append(" rel.modifier, ");
+		SQL.append(" rel.last_modified ");
+		SQL.append("FROM gp_tag_rel rel ");
+		SQL.append(" LEFT JOIN gp_tags tag on (rel.category = tag.category and rel.tag_name = tag.tag_name) ");
+		SQL.append("WHERE rel.resource_type = :obj_type and rel.resource_id in ( :obj_ids ) ");
+		SQL.append("ORDER BY rel.resource_type, rel.resource_id ");
+		
+		if(ArrayUtils.isEmpty(objectIds)){
+			return result;
+		}else{
+			String res_type = objectIds[0].getIdKey();
+			List objIds = new ArrayList();
+			for(InfoId<?> objId : objectIds){
+				objIds.add(objId.getId());
+			}
+			paramap.put("obj_ids", objIds);
+			paramap.put("obj_type", res_type);
+		}
+		
+		try{
+			NamedParameterJdbcTemplate jtemplate = pseudodao.getJdbcTemplate(NamedParameterJdbcTemplate.class);
+			if(LOGGER.isDebugEnabled()){
+				LOGGER.debug("SQL : {} / PARAMS : {}", SQL.toString(), paramap.toString());
+			}
+			final RowMapper<TagInfo> TAG_MAPPER = tagdao.getRowMapper();
+			jtemplate.query(SQL.toString(), paramap, new RowCallbackHandler(){
+				
+				/**
+				 * process every row and add it to Result Map(map key is the resource id) 
+				 **/
+				public void processRow(ResultSet rs) throws SQLException {
+					String res_type = rs.getString("resource_type");
+					Long res_id = rs.getLong("resource_id");
+					Identifier idfier = IdKey.valueOfIgnoreCase(res_type);
+					
+					InfoId<Long> resid = new InfoId<Long>(idfier, res_id);
+					Set<TagInfo> tags = result.get(resid);
+					if(tags == null){
+						result.put(resid, new HashSet<TagInfo>());
+					}
+					TagInfo tag = TAG_MAPPER.mapRow(rs, 0);
+					tags.add(tag);
+				}
+			});
+			
+		}catch(DataAccessException dae){
+			throw new ServiceException("fail query tags",dae);
+		}
+		return result;
+		
+	}
+	
 	@Transactional(value = ServiceConfigurer.TRNS_MGR)
 	@Override
 	public boolean newTag(ServiceContext<?> svcctx, TagInfo taginfo) throws ServiceException {
