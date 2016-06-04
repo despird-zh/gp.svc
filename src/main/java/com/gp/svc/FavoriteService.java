6eb1e15f89d7aa80d3f49837b37aa7cad626c0db
gary <gary.diao@yahoo.com>
2016-06-04 17:31:23 +0800

    update interface

--- src/main/java/com/gp/svc/FavoriteService.java
+++ src/main/java/com/gp/svc/FavoriteService.java
@@ -1,5 +1,22 @@
 package com.gp.svc;
 
+import java.util.List;
+import java.util.Map;
+
+import com.gp.common.ServiceContext;
+import com.gp.exception.ServiceException;
+import com.gp.info.FavoriteInfo;
+import com.gp.info.InfoId;
+
 public interface FavoriteService {
 
+	public Map<InfoId<Long>, Integer> getFavFileSummary(ServiceContext<?> svcctx,  List<InfoId<Long>>infoids )throws ServiceException;
+	
+	public Map<InfoId<Long>, Integer> getFavFolderSummary(ServiceContext<?> svcctx, List<InfoId<Long>>infoids )throws ServiceException;
+	
+	public List<FavoriteInfo> getFavorites(ServiceContext<?> svcctx,String type, String favoriter)throws ServiceException;
+	
+	public boolean addFavorite(ServiceContext<?> svcctx, FavoriteInfo fav)throws ServiceException;
+	
+	public boolean removeFavorite(ServiceContext<?> svcctx, String favoriter, InfoId<Long> resourceId)throws ServiceException;
 }
