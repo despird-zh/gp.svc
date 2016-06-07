a20cd326e6e8a2ae11a08448ed28f9c22ca4d135
gary <gary.diao@yahoo.com>
2016-06-08 00:35:39 +0800

    f

--- src/main/java/com/gp/info/DictionaryInfo.java
+++ src/main/java/com/gp/info/DictionaryInfo.java
@@ -19,8 +19,18 @@ public class DictionaryInfo extends TraceableInfo<Long>{
 	private String label;
 
 	@NotNull
-	private String language;
+	private String defaultLanguage;
 	
+	private String labelzh_CN;
+	
+	public String getDefaultLanguage() {
+		return defaultLanguage;
+	}
+
+	public void setDefaultLanguage(String defaultLanguage) {
+		this.defaultLanguage = defaultLanguage;
+	}
+
 	public String getGroup() {
 		return group;
 	}
@@ -52,14 +62,5 @@ public class DictionaryInfo extends TraceableInfo<Long>{
 	public void setLabel(String label) {
 		this.label = label;
 	}
-
-	public String getLanguage() {
-		return language;
-	}
-
-	public void setLanguage(String language) {
-		this.language = language;
-	}
-
 		
 }
