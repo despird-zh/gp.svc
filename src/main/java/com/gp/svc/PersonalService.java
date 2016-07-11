46165cd5a6b934d644fe358c69164a4cc554cd47
gary-dgc <garydiao@localhost>
2016-07-11 17:13:42 +0800

    update get user workgroups

--- src/main/java/com/gp/svc/PersonalService.java
+++ src/main/java/com/gp/svc/PersonalService.java
@@ -4,34 +4,50 @@ import java.util.List;
 
 import com.gp.common.ServiceContext;
 import com.gp.exception.ServiceException;
+import com.gp.info.ChatMessageInfo;
 import com.gp.info.GroupMemberInfo;
-import com.gp.info.InfoId;
 import com.gp.info.OrgHierInfo;
 import com.gp.info.UserSumInfo;
 import com.gp.info.WorkgroupInfo;
+import com.gp.pagination.PageQuery;
 
 public interface PersonalService {
 
-	public List<WorkgroupInfo> getWorkgroups(ServiceContext svcctx, InfoId<?>... ids)throws ServiceException;
+	/**
+	 * Get the work groups which user joined
+	 * @param account the account information
+	 **/
+	public List<WorkgroupInfo> getWorkgroups(ServiceContext svcctx, String account)throws ServiceException;
 	
-	public List<OrgHierInfo> getOrgHierNodes(ServiceContext svcctx, InfoId<?>... ids)throws ServiceException;
+	/**
+	 * Get the orghier which user joined
+	 * @param account the account information
+	 **/
+	public List<OrgHierInfo> getOrgHierNodes(ServiceContext svcctx, String account)throws ServiceException;
 	
 	/**
 	 * Get the work group member belonging relationship by account.
 	 * this helps to find the work group belongs of a user.
-	 * 
 	 * @param account the account information
 	 **/
 	public List<GroupMemberInfo> getWorkgroupMembers(ServiceContext svcctx, String account) throws ServiceException;
+	
 	/**
 	 * Get the org hier member belonging relationship by account
 	 * this helps to find the org hier belongs of a user.
-	 * 
 	 * @param account the account information
 	 **/
 	public List<GroupMemberInfo> getOrgHierMembers(ServiceContext svcctx, String account) throws ServiceException;
 	
+	/**
+	 * Get the user summary information
+	 * @param account the account information 
+	 **/
 	public UserSumInfo getUserSummary(ServiceContext svcctx, String account) throws ServiceException;
-
-	//public List<MessageInfo> getMessage
+	
+	/**
+	 * Get the user summary information
+	 * @param account the account information 
+	 **/
+	public List<ChatMessageInfo> getChatMessages(ServiceContext svcctx, String account, PageQuery pquery)throws ServiceException;
 }
--- src/main/java/com/gp/svc/impl/PersonalServiceImpl.java
+++ src/main/java/com/gp/svc/impl/PersonalServiceImpl.java
@@ -9,11 +9,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.gp.common.GroupUsers.GroupType;
+import com.gp.common.IdKey;
 import com.gp.common.ServiceContext;
 import com.gp.config.ServiceConfigurer;
 import com.gp.dao.GroupUserDAO;
@@ -22,11 +24,13 @@ import com.gp.dao.PseudoDAO;
 import com.gp.dao.UserSumDAO;
 import com.gp.dao.WorkgroupDAO;
 import com.gp.exception.ServiceException;
+import com.gp.info.ChatMessageInfo;
 import com.gp.info.GroupMemberInfo;
 import com.gp.info.InfoId;
 import com.gp.info.OrgHierInfo;
 import com.gp.info.UserSumInfo;
 import com.gp.info.WorkgroupInfo;
+import com.gp.pagination.PageQuery;
 import com.gp.svc.PersonalService;
 
 @Service("personalService")
@@ -48,9 +52,29 @@ public class PersonalServiceImpl implements PersonalService{
 	
 	@Transactional(value = ServiceConfigurer.TRNS_MGR, readOnly = true)
 	@Override
-	public List<WorkgroupInfo> getWorkgroups(ServiceContext svcctx, InfoId<?>... ids) throws ServiceException {
+	public List<WorkgroupInfo> getWorkgroups(ServiceContext svcctx, String account) throws ServiceException {
 		
+		StringBuffer SQL = new StringBuffer("SELECT * FROM gp_workgroup_mbrs ");
+		SQL.append("WHERE account = ? AND group_type = '").append(GroupType.WORKGROUP_MBR.name()).append("'");
+		
+		Object[] params = new Object[]{
+			account
+		};
+
+		JdbcTemplate jtemplate = pseudodao.getJdbcTemplate(JdbcTemplate.class);
+		
+		if(LOGGER.isDebugEnabled()){
+			LOGGER.debug("SQL : {} / PARAMS : {}",SQL.toString(), params.toString());
+		}
+		InfoId<?>[] ids = null;
 		try{
+			List<GroupMemberInfo> members = jtemplate.query(SQL.toString(), params, GroupUserDAO.GroupMemberMapper);
+			ids = new InfoId<?>[members.size()];
+			int count = 0;
+			for(GroupMemberInfo minfo : members){
+				ids[count] = IdKey.WORKGROUP.getInfoId(minfo.getManageId());
+				count ++;
+			}
 			return workgroupdao.queryByIds(ids);
 		}catch(DataAccessException dae){
 			throw new ServiceException("excp.query.with", dae, "workgroup", Arrays.toString(ids));
@@ -59,9 +83,29 @@ public class PersonalServiceImpl implements PersonalService{
 
 	@Transactional(value = ServiceConfigurer.TRNS_MGR, readOnly = true)
 	@Override
-	public List<OrgHierInfo> getOrgHierNodes(ServiceContext svcctx, InfoId<?>... ids) throws ServiceException {
-
+	public List<OrgHierInfo> getOrgHierNodes(ServiceContext svcctx, String account) throws ServiceException {
+		StringBuffer SQL = new StringBuffer("SELECT * FROM gp_orghier_mbrs ");
+		SQL.append("WHERE account = ? AND group_type = '").append(GroupType.ORG_HIER_MBR.name()).append("'");
+		
+		Object[] params = new Object[]{
+				account
+			};
+		
+		JdbcTemplate jtemplate = pseudodao.getJdbcTemplate(JdbcTemplate.class);
+		
+		if(LOGGER.isDebugEnabled()){
+			LOGGER.debug("SQL : {} / PARAMS : {}",SQL.toString(), params.toString());
+		}
+		InfoId<?>[] ids = null;
 		try{
+			List<GroupMemberInfo> members = jtemplate.query(SQL.toString(), params, GroupUserDAO.GroupMemberMapper);
+			ids = new InfoId<?>[members.size()];
+			int count = 0;
+			for(GroupMemberInfo minfo : members){
+				ids[count] = IdKey.WORKGROUP.getInfoId(minfo.getManageId());
+				count ++;
+			}
+			
 			return orghierdao.queryByIds(ids);
 		}catch(DataAccessException dae){
 			throw new ServiceException("excp.query.with", dae, "orghier", Arrays.toString(ids));
@@ -99,7 +143,7 @@ public class PersonalServiceImpl implements PersonalService{
 	public List<GroupMemberInfo> getOrgHierMembers(ServiceContext svcctx, String account)
 			throws ServiceException {
 		
-		StringBuffer SQL = new StringBuffer("SELECT * FROM gp_workgroup_mbrs ");
+		StringBuffer SQL = new StringBuffer("SELECT * FROM gp_orghier_mbrs ");
 		SQL.append("WHERE account = :account AND group_type = '").append(GroupType.ORG_HIER_MBR.name()).append("'");
 		
 		Map<String, Object> params = new HashMap<String, Object>();
@@ -116,7 +160,7 @@ public class PersonalServiceImpl implements PersonalService{
 		
 		}catch(DataAccessException dae){
 			
-			throw new ServiceException("excp.query.with", dae, "group member", params.toString());
+			throw new ServiceException("excp.query.with", dae, "orghier member", params.toString());
 		}
 	}
 	
@@ -131,6 +175,14 @@ public class PersonalServiceImpl implements PersonalService{
 		}catch(DataAccessException dae){
 			throw new ServiceException("excp.query.with", dae, "user sumamry", account);
 		}
+	}
+
+	@Transactional(value = ServiceConfigurer.TRNS_MGR, readOnly = true)
+	@Override
+	public List<ChatMessageInfo> getChatMessages(ServiceContext svcctx, String account, PageQuery pquery)
+			throws ServiceException {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 }
