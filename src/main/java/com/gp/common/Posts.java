9a228358b5c769e05586274ee912a28a459e9f82
gary <gary.diao@yahoo.com>
2016-07-26 00:10:30 +0800

    add scope to post table

--- src/main/java/com/gp/common/Posts.java
+++ src/main/java/com/gp/common/Posts.java
@@ -3,6 +3,12 @@ package com.gp.common;
 public class Posts {
 	public static enum Scope{
 		SQUARE, // publish to square
-		WGROUP  // publish to work group internal only
+		WGROUP,  // publish to work group internal only
+		PRIVATE, // only visible to attendee
+	}
+	
+	public static enum Type{
+		DISCUSSION,
+		VOTING,
 	}
 }
