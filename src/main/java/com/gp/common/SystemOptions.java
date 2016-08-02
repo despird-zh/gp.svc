90a5130ff2cbd8bbae71eee2cb970441f5dd7337
gary <gary.diao@yahoo.com>
2016-08-02 20:36:50 +0800

    update system option

--- src/main/java/com/gp/common/SystemOptions.java
+++ src/main/java/com/gp/common/SystemOptions.java
@@ -17,17 +17,17 @@ package com.gp.common;
 
 public class SystemOptions {
 
-	public static final String PERSONAL_CABINET_QUOTA = "PERSONAL_CABINET_QUOTA";
-	public static final String WORKGROUP_CABINET_QUOTA = "WORKGROUP_CABINET_QUOTA";
-	public static final String QUERY_MAX_ROWS = "QUERY_MAX_ROWS";
-	public static final String SYSTEM_OPTION_TTL = "SYSTEM_OPTION_TTL";
-	public static final String SECURITY_HASH_SALT = "SECURITY_HASH_SALT";
-	public static final String AUDIT_ENABLE = "AUDIT_ENABLE";
-	public static final String IMAGE_CACHE_PATH = "IMAGE_CACHE_PATH";
-	public static final String SYSTEM_VERSION = "SYSTEM_VERSION";
-	public static final String SYSTEM_APP = "SYSTEM_APP";
-	public static final String VALID_MSG_RESOURCES = "VALID_MSG_RESOURCES";
-	public static final String CABINET_VERSION_ENABLE = "CABINET_VERSION_ENABLE";
+	public static final String PERSONAL_CABINET_QUOTA = "personal.cabinet.quota";
+	public static final String WORKGROUP_CABINET_QUOTA = "workgroup.cabinet.quota";
+	public static final String QUERY_MAX_ROWS = "query.max.rows";
+	public static final String SYSTEM_OPTION_TTL = "system.option.ttl";
+	public static final String SECURITY_HASH_SALT = "security.hash.salt";
+	public static final String AUDIT_ENABLE = "audit.enable";
+	public static final String IMAGE_CACHE_PATH = "image.cache.path";
+	public static final String SYSTEM_VERSION = "system.version";
+	public static final String SYSTEM_APP = "system.app";
+	public static final String VALID_MSG_RESOURCES = "valid.msg.resources";
+	public static final String CABINET_VERSION_ENABLE = "cabinet.version.enable";
 	public static final String FILE_CACHE_PATH = "file.cache.path";
 
 }
