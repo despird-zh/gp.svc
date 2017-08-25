0dec13d0e046fd86ba06ae84dd668833f4cc354a
admin <admin@192.168.0.21>
2017-08-25 14:24:32 +0800

    update context utils

--- src/main/java/com/gp/common/SpringContextUtil.java
+++ src/main/java/com/gp/common/SpringContextUtil.java
@@ -21,33 +21,64 @@ import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 
+/**
+ * Class to hold the ApplicationContext instance
+ * 
+ * @author diaogc
+ * @version 0.1 2016-12-11
+ * 
+ **/
 public class SpringContextUtil implements ApplicationContextAware {
 
 	Logger LOGGER = LoggerFactory.getLogger(SpringContextUtil.class);
 	
 	private static ApplicationContext applicationContext;
 
+	/**
+	 * Method to set the ApplicationContext
+	 **/
 	public void setApplicationContext(ApplicationContext context) throws BeansException {
 		applicationContext = context;
 	}
 
+	/**
+	 * Method to get the ApplicationContext 
+	 **/
 	public static ApplicationContext getApplicationContext() {
 		return applicationContext;
 	}
 	
-	@SuppressWarnings("unchecked")
+	/**
+	 * Get the Spring Bean instance 
+	 * @param beanname the name of bean
+	 * @param clazz the bean class
+	 **/
 	public static <T> T getSpringBean(String beanname, Class<T> clazz){
 		
 		if(null == applicationContext) return null;
 		
-		Object bean = applicationContext.getBean(beanname);
-		return (T) bean;
+		T bean = applicationContext.getBean(beanname, clazz);
+		return  bean;
 	}
 	
+	/**
+	 * Get the Spring Bean instance 
+	 **/
 	public static <T> T getSpringBean(Class<T> clazz){
 		
 		if(null == applicationContext) return null;
 		
 		return applicationContext.getBean(clazz);
+	}
+	
+	/**
+	 * Bind the target bean with autowire annotated properties 
+	 * @param existingBean 
+	 **/
+	public static void bindAutowire(Object existingBean) {
+		
+		if(null == applicationContext) return;
+		
+		applicationContext.getAutowireCapableBeanFactory().autowireBean(existingBean);
 	}
 }
